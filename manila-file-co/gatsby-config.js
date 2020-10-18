22b5ded6556b674e79fdb9d7958fc20f46781c51
ben <bengg@mac.com>
2020-10-17 22:07:40 -0700

    update url

--- manila-file-co/gatsby-config.js
+++ manila-file-co/gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
         // Any additional optional fields
         sampleRate: 5,
         siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
+        cookieDomain: "manilafile.co",
       },
     },
   ],

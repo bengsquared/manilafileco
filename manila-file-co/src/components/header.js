c1279c47be8034a6df584238c71fdf691c02284d
ben <bengg@mac.com>
2020-10-18 09:06:08 -0700

    update for responsive

--- manila-file-co/src/components/header.js
+++ manila-file-co/src/components/header.js
@@ -4,20 +4,11 @@ import logo from "../img/manila.svg"
 export default function Header({ currentPage }) {
   return (
     <header>
-      <div
-        style={{ gridColumnStart: "2", gridColumnEnd: "6" }}
-        className="header-title"
-      >
+      <div className="header-title">
         <img alt="Manila logo" className="header-icon" src={logo} />
         <h4>Manila File Co</h4>
       </div>
-      <nav
-        style={{
-          gridColumnStart: "6",
-          gridColumnEnd: "12",
-        }}
-        className="header-nav"
-      >
+      <nav className="header-nav">
         <a href="https://www.notion.so/FileGraph-Tag-based-file-manager-8529875abc62435a9232b4cf1b45520b">
           <h4>About</h4>
         </a>
--- manila-file-co/src/pages/index.js
+++ manila-file-co/src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <section className="splash">
           <img alt="Manila logo" src={logo} className="BigImage center" />
           <h1>Manila File Company</h1>
-          <h3>tools and ideas for the modern file system</h3>
+          <h2>tools and ideas for the modern file system</h2>
         </section>
         <section className="body-section">
           <p>
@@ -23,22 +23,32 @@ export default function Home() {
           </p>
         </section>
         <section id="tools" className="body-section">
-          <h4>Tools</h4>
+          <h3>Tools</h3>
           <hr />
         </section>
         <section className="grid-list">
           <img alt="tidyUp logo" className="gl-icon" src={tidyUp} />
           <div className="gl-name">
-            <h5>
-              <span className="red-title">tidyUp</span> helps you clean up your
-              desktop and other large folders
-            </h5>
+            <h4>
+              <span className="red-title">{"tidyUp "}</span>
+              helps you clean up your desktop and other large folders
+            </h4>
           </div>
-          <div className="gl-button1">Coming soon</div>
+          <div className="gl-button1">
+            <h5>Coming soon</h5>
+          </div>
+        </section>
+        <section className="body-section center">
+          <h5>
+            {"Want to hear when we launch more stuff? "}
+            <a href="https://www.producthunt.com/upcoming/manilla">
+              Sign up here for updates!
+            </a>
+          </h5>
         </section>
       </main>
       <footer>
-        <ul style={{ gridColumnStart: "4", gridColumnEnd: "6" }}>
+        <ul id="address">
           <li>
             <h4>Manila File Co.</h4>
           </li>
@@ -46,7 +56,7 @@ export default function Home() {
             <h4>Made in California</h4>
           </li>
         </ul>
-        <ul style={{ gridColumnStart: "8", gridColumnEnd: "10" }}>
+        <ul id="aboutcontact">
           <li>
             <a href="https://www.notion.so/FileGraph-Tag-based-file-manager-8529875abc62435a9232b4cf1b45520b">
               <h4>About</h4>
@@ -55,6 +65,11 @@ export default function Home() {
           <li>
             <a href="mailto:bengg@mac.com">
               <h4>Contact</h4>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.producthunt.com/upcoming/manilla">
+              <h4>Subscribe</h4>
             </a>
           </li>
         </ul>
